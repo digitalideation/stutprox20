5777546a8388e49f489d3d014fd21c502c41b4c7
Gordan Savicic <gordan.savicic@gmail.com>
2020-03-19 14:13:03 +0100

    added 2 examples

---
+++ examples/Accel_2Stepper/Accel_2Stepper.ino
@@ -0,0 +1,66 @@
+// Shows how to run three Steppers at once with varying speeds
+//
+// Requires the Adafruit_Motorshield v2 library
+//   https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
+// And AccelStepper with AFMotor support
+//   https://github.com/adafruit/AccelStepper
+
+// This tutorial is for Adafruit Motorshield v2 only!
+// Will not work with v1 shields
+
+#include <Wire.h>
+#include <AccelStepper.h>
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
+
+// Connect two steppers with 200 steps per revolution (1.8 degree)
+// to the top shield
+Adafruit_StepperMotor *myStepper1 = AFMStop.getStepper(200, 1);
+Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
+
+
+// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+// wrappers for the first motor!
+void forwardstep1() {
+  myStepper1->onestep(FORWARD, SINGLE);
+}
+void backwardstep1() {
+  myStepper1->onestep(BACKWARD, SINGLE);
+}
+// wrappers for the second motor!
+void forwardstep2() {
+  myStepper2->onestep(FORWARD, SINGLE );
+}
+void backwardstep2() {
+  myStepper2->onestep(BACKWARD, SINGLE);
+}
+
+AccelStepper stepper1(forwardstep1, backwardstep1);
+AccelStepper stepper2(forwardstep2, backwardstep2);
+
+void setup()
+{
+  AFMStop.begin(); // Start the top shield
+
+  stepper1.setMaxSpeed(200.0);
+  stepper1.setAcceleration(100.0);
+  stepper1.moveTo(24);
+
+  stepper2.setMaxSpeed(200.0);
+  stepper2.setAcceleration(200.0);
+  stepper2.moveTo(500);
+}
+
+void loop()
+{
+  // Change direction at the limits
+  if (stepper1.distanceToGo() == 0)
+    stepper1.moveTo(-stepper1.currentPosition());
+
+  if (stepper2.distanceToGo() == 0)
+    stepper2.moveTo(-stepper2.currentPosition());
+
+  stepper1.run();
+  stepper2.run();
+}
