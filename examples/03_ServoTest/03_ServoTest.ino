a43643de7400acf5ad23fce0d0be611308fa9643
Gordan Savicic <gordan.savicic@gmail.com>
2020-03-24 22:59:09 +0100

    updated servo example

--- examples/03_ServoTest/03_ServoTest.ino
+++ examples/03_ServoTest/03_ServoTest.ino
@@ -1,30 +1,24 @@
 /*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
-
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
+  Moves the Servo attached to pin 10 from 0 to 180 degrees and backwards
+  Gordan Savicic, March 2020
 */
 
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val = 150;    // variable to read the value from the analog pin
+int val = 0;
+int counter = 1;
 
 void setup() {
   Serial.begin(115200);
-  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
-  //myservo.write(0);   
+  myservo.attach(10);  // attaches the servo on pin 10
 }
 
 void loop() {
-  // myservo.write(90);
+  val += counter;
   Serial.println(val);
-  val += 0;    // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
- if (val % 180 == 0) val = 0;
+  myservo.write(val); // sets the servo position according to the scaled value
+  delay(15);  // waits for the servo to get there
+  if (val > 179 || val < 1) counter = -counter;
 }
